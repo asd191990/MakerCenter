43a71f42a79cde9acba738b577b8286394bb602d
asd191990 <asd19199009@gmail.com>
2022-02-08 13:57:19 +0800

    change model

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -1,15 +1,18 @@
 import imp
 from importlib.resources import contents
+from turtle import update
 from django.db import models
 from django.utils import timezone
 
-#最新消息
+# 最新消息
+
 class News(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     date = models.DateField(default=timezone.now)
 
-#課程(專業、核心)
+# 課程(專業、核心)
+
 class Coures(models.Model):
     Coure_type = (
         ('1', '專業實作課程'),
@@ -20,17 +23,19 @@ class Coures(models.Model):
     type = models.CharField(max_length=1, choices=Coure_type)
     date = models.DateField(default=timezone.now)
 
-#校級共用實驗室借用情形
+# 校級共用實驗室借用情形
+
 class classroomintroducts(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     date = models.DateField(default=timezone.now)
-       # 可能有照片要加 但先這樣
+    imageone = models.ImageField(upload_to="classimage")
+    # 可能有照片要加 但先這樣
 
-#各類申請及說明
+# 各類申請及說明
+
 class DownLoadFiles(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    filepath = models.URLField(max_length=200)
+    filepath = models.FileField(upload_to='uploads')
     date = models.DateField(default=timezone.now)
-
