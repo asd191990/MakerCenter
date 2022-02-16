538d1b42eddd334080e340ccd0f0f302e21f2f71
asd191990 <asd19199009@gmail.com>
2022-02-16 15:33:01 +0800

    new show test

--- MakerCenter/settings.py
+++ MakerCenter/settings.py
@@ -171,10 +171,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media/'
 # MEDIA_ROOT = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
-# CKEDITOR_UPLOAD_PATH = 'upload/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
 
+CKEDITOR_UPLOAD_PATH = 'upload/'
+CKEDITOR_IMAGE_BACKEND = 'pillow'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
