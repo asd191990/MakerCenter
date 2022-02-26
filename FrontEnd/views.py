21842ebf96bf39b8fe5dc76d95962edbefe2bb5d
asd191990 <asd19199009@gmail.com>
2022-02-26 19:14:40 +0800

    顯示資料處理

--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "FrontEnd/index/index.html")
+
+
+def single(request):
+    return render(request, "FrontEnd/base_single/base_single.html")
