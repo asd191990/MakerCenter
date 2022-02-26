ea2b4a36602ec7aabba28ca991af12b1a5f3cd87
asd191990 <asd19199009@gmail.com>
2022-02-26 23:21:19 +0800

    course_list 設定調整範例

--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, "FrontEnd/index/index.html")
+def courselist(request):
+    return render(request, "FrontEnd/course/course_list.html")
