abb21e723f6fa34d7c99e519a0ec91ed51f3906f
kioap1002 <b11023060@gemail.yuntech.edu.tw>
2022-02-27 16:21:02 +0800

    空間介紹/全部消息/urls/views

--- FrontEnd/urls.py
+++ FrontEnd/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('course_list', views.courselist, name='course_list'),
+    path('course_list/', views.courselist, name='course_list'),
+    path('base_single/', views.basesingle, name='base_single'),
+    path('course_page/', views.coursepage, name='course_page'),
+    path('position/', views.position, name='position'),
+    path('news_list/', views.newslist, name='news_list'),
+    path('space_intro/', views.spaceintro, name='space_intro'),
 ]
--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -6,3 +6,13 @@ def index(request):
     return render(request, "FrontEnd/index/index.html")
 def courselist(request):
     return render(request, "FrontEnd/course/course_list.html")
+def basesingle(request):
+    return render(request, "FrontEnd/base_single/base_single.html")
+def coursepage(request):
+    return render(request, "FrontEnd/course_page/course_page.html")
+def position(request):
+    return render(request, "FrontEnd/position/position.html")
+def newslist(request):
+    return render(request, "FrontEnd/news_list/news_list.html")
+def spaceintro(request):
+    return render(request, "FrontEnd/space_intro/space_intro.html")
