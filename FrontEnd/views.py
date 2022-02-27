f557367f03763345f85e8e14ef4a083dd3cd86a4
asd191990 <asd19199009@gmail.com>
2022-02-27 16:51:47 +0800

    完成教室搜尋

--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -1,4 +1,3 @@
-from calendar import c
 from django.shortcuts import render
 
 # Create your views here.
@@ -21,10 +20,5 @@ def spaceintro(request):
 def single(request):
     return render(request, "FrontEnd/base_single/base_single.html")
 def courselist(request):
-    x = dict()
-    x["date"] = "23"
-    x["type"] = "213"
-    template = get_template("FrontEnd/course/classroom_box.html")
-    html = template.render({"data": x})
-    print(html)
+    
     return render(request, "FrontEnd/course/course_list.html")
