f59c4624267ccafec088c90e0052602ac85a9e11
kioap1002 <b11023060@gemail.yuntech.edu.tw>
2022-03-04 20:18:19 +0800

    新增:成員/設備/辦法  增加:變色/流動

--- FrontEnd/urls.py
+++ FrontEnd/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('space_intro/', views.spaceintro, name='space_intro'),
     path('course_list/', views.courselist, name='course_list'),
     path('single/', views.single, name='single'),
+    path('equipment_intro/', views.equipmentintro, name='equipment_intro'),
+    path('members_intro/', views.membersintro, name='members_intro'),
+    path('download/', views.download, name='download'),
 ]
--- FrontEnd/views.py
+++ FrontEnd/views.py
@@ -15,7 +15,12 @@ def newslist(request):
     return render(request, "FrontEnd/news_list/news_list.html")
 def spaceintro(request):
     return render(request, "FrontEnd/space_intro/space_intro.html")
-
+def equipmentintro(request):
+    return render(request, "FrontEnd/equipment_intro/equipment_intro.html")
+def membersintro(request):
+    return render(request, "FrontEnd/members_intro/members_intro.html")
+def download(request):
+    return render(request, "FrontEnd/download/download.html")
 
 def single(request):
     return render(request, "FrontEnd/base_single/base_single.html")
