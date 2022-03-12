0cd3d22cc9592c866f88e35ff7d13245fc8edf75
asd191990 <asd19199009@gmail.com>
2022-03-13 02:02:16 +0800

    課程列表 完成前往單頁的導引

--- FrontEnd/urls.py
+++ FrontEnd/urls.py
@@ -12,14 +12,13 @@ urlpatterns = [
     path('news_list/', views.newslist, name='news_list'),
     path('space_intro/', views.spaceintro, name='space_intro'),
     path('course_list/', views.courselist, name='course_list'),
-    # path('single/', views.single, name='single'),
     path('equipment_intro/', views.equipmentintro, name='equipment_intro'),
     path('members_intro/', views.membersintro, name='members_intro'),
     path('download/', views.download, name='download'),
     # 相關辦法 下載
     path('download/<int:getid>',views.downloadFile, name='download_file'),
     #分頁
-    path('single/<str:dbtype>/<int:id>', views.basesingle, name='single'),
+    path('single/<str:dbtype>/<int:id>', views.basesingle, name='thesingle'),
     #專業小組
     path('class/<int:id>', views.classshow, name='classshow'),
 ]
