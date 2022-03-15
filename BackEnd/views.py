565fd833d17d7e30adf81421a6a1640bb2695eba
shao0213 <z48551044@gmail.com>
2022-03-16 00:56:14 +0800

    course_list顯示圖片及型態

--- BackEnd/views.py
+++ BackEnd/views.py
@@ -4,65 +4,39 @@ from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
 from .forms import NewsForm
 from .models import News,Course,Group,DownLoadFiles,Memebers,Equipment,Space
 from .filters import NewsFilter
-
+from django.views.decorators.csrf import csrf_exempt
 from .fuc import DBprocess
 
-
+@csrf_exempt
 def DBlist(request,dbtype):
-    datalist=[]
-    if dbtype=="DownLoadFiles":
-        getlist = DownLoadFiles.objects.all()
-        for v in getlist:
-            v.typeshow = v.get_type_display()
-            v =  v.__dict__        
-            del v['_state']
-            del v['filepath']
-            del v['_django_cleanup_original_cache']
-            datalist.append(v)
+    print(dbtype)
+    getdb =DBprocess(dbtype)
+    datalist = []
+    if dbtype ==None:
+        return JsonResponse({"error":"請求失敗"})
+    else:        
+        getdata = getdb.objects.all()
+        for one_data in getdata:
+            if one_data.get_type_display() != None:
+                one_data.type = one_data.get_type_display()
+                # print(one_data.typeshow)
+            data = one_data.to_dict()
+            print("-------------------")
+            print(data)
+            datalist.append(data)
         print(datalist)
-    
-    elif   dbtype=="Memebers":
-        getlist = Memebers.objects.all()
-        for v in getlist:
-            v =  v.__dict__
-            del v['_state']
-            datalist.append(v)
-        print(datalist)
-    
-    elif   dbtype=="News":
-        getlist = News.objects.all()
-        for v in getlist:
-            v =  v.__dict__
-            del v['_state']
-            datalist.append(v)
-        print(datalist)
-    elif   dbtype=="Equipment":
-        getlist = Equipment.objects.all()
-        for v in getlist:
-            v =  v.__dict__
-            del v['_state']
-            datalist.append(v)
-        print(datalist)
-    elif   dbtype=="Space":
-        getlist = Space.objects.all()
-        for v in getlist:
-            v =  v.__dict__
-            del v['_state']
-            datalist.append(v)
-        print(datalist)
+    return JsonResponse(datalist, safe=False)
+    #     getlist = Course.objects.all()
+    #     print(getlist)
+    #     datalist = []
+    #     for v in getlist:
+    #         v.typeshow = v.get_type_display()
+    #         v =  v.__dict__
+    #         del v['_state']
+    #         del v['image']
+    #         del v['_django_cleanup_original_cache']
+    #         datalist.append(v)
+    #     print(datalist)
+    # return JsonResponse(datalist,safe=False)
 
-    else:
-        print(dbtype)
-        
-        getlist = Course.objects.all()
-        print(getlist)
-        datalist = []
-        for v in getlist:
-            v.typeshow = v.get_type_display()
-            v =  v.__dict__
-            del v['_state']
-            del v['image']
-            del v['_django_cleanup_original_cache']
-            datalist.append(v)
-        print(datalist)
-    return JsonResponse(datalist,safe=False)
+    
