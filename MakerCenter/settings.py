56718b3420ac58d557b4b01124dd0d5da1c295d3
asd191990 <asd19199009@gmail.com>
2022-03-24 00:35:43 +0800

    Update settings.py

--- MakerCenter/settings.py
+++ MakerCenter/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7%p9qm@(jy0t1zh)38v@@jjmg*6r2)da6%%bzgp4k(g&_p6$2r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
